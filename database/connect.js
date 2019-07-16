ba745861eec26183011323ce158b7bdb4c5a46fb
FernandoLFMC <ferchitolfmc@gmail.com>
2019-07-16 19:47:08 -0400

    rename

--- database/connect.js
+++ database/connect.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://172.20.0.2:27017/servicioAFA");
+mongoose.connect("mongodb://172.24.0.2:27017/servicioAFA");
 module.exports = mongoose;
